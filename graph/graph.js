0baca3df36d070b1ff03d66a14fc7cd5c9b79c51
Lia Chen <scunchen@ad.isr.umich.edu>
2020-03-12 17:36:46 -0400

    modify data structure

--- graph/graph.js
+++ graph/graph.js
@@ -8,7 +8,7 @@ class Graph {
         this.AdjList.set(v, []);
     }
 
-    addEdge(v,w) {
+    addEdge(v, w) {
         this.AdjList.get(v).push(w);
         this.AdjList.get(w).push(v);
     }
@@ -28,15 +28,16 @@ class Graph {
     //main DFS method
     dfs(startingNode) {
         let visited = [];
-        for (let i = 0; i < this.noOfVertices; i++) {
-            visited[i] = false;
+        for (let key of this.AdjList.keys()) {
+            visited[key] = false;
         }
-        this.dfsUtil(v, visited);
+        this.dfsUtil(startingNode, visited);
     }
 
     //recursive function which processes and explore all the adjacent vertex of the vertex with which it is called
     dfsUtil(v, visited) {
         visited[v] = true;
+        console.log(v)
         let neighbors = this.AdjList.get(v);
         for (let neighbor of neighbors) {
             if (!visited[neighbor]) {
@@ -46,4 +47,31 @@ class Graph {
     }
 
 }
+var g = new Graph(6);
 
+var vertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+// adding vertices 
+for (var i = 0; i < vertices.length; i++) {
+    g.addVertex(vertices[i]);
+}
+
+// adding edges 
+g.addEdge('A', 'B');
+g.addEdge('A', 'D');
+g.addEdge('A', 'E');
+g.addEdge('B', 'C');
+g.addEdge('D', 'E');
+g.addEdge('E', 'F');
+g.addEdge('E', 'C');
+g.addEdge('C', 'F');
+// prints all vertex and 
+// its adjacency list 
+// A -> B D E 
+// B -> A C 
+// C -> B E F 
+// D -> A E 
+// E -> A D F C 
+// F -> E C 
+g.printGraph();
+g.dfs('A'); 
