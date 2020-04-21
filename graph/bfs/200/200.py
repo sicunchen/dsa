7803d8959d0df68a24b78e52179f6313d40f8123
depechecode <scunchen@umich.edu>
2020-04-20 23:57:17 -0400

    update lc200

--- graph/bfs/200/200.py
+++ graph/bfs/200/200.py
@@ -2,48 +2,35 @@
 
 
 class Solution:
-    """
-    @param grid: a boolean 2D matrix
-    @return: an integer
-    """
-
-    def numIslands(self, grid):
-        # write your code here
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
             return 0
 
         islands = 0
-        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] and (i, j) not in visited:
-                    self.bfs(grid, i, j, visited)
+                if grid[i][j] == "1":
+                    self.bfs(grid, i, j)
                     islands += 1
 
         return islands
 
-    def bfs(self, grid, x, y, visited):
-        queue = collections.deque([(x, y)])
-        # 圆括号传的是tuple,value copy
-        visited.add((x, y))
+    def bfs(self, grid, x, y):
+        queue = deque([(x, y)])
+        grid[x][y] = "0"
         while queue:
             x, y = queue.popleft()
-            # 坐标变换数组
             for delta_x, delta_y in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
                 next_x = x + delta_x
                 next_y = y + delta_y
-                if not self.is_valid(grid, next_x, next_y, visited):
+                if not self.is_valid(grid, next_x, next_y):
                     continue
                 queue.append((next_x, next_y))
-                visited.add((next_x, next_y))
+                grid[next_x][next_y] = "0"
 
-    def is_valid(self, grid, x, y, visited):
+    def is_valid(self, grid, x, y):
         n, m = len(grid), len(grid[0])
-        if not (0 <= x < n and 0 <= y < m):
-            return False
-        if (x, y) in visited:
-            return False
-        return grid[x][y]
+        return 0 <= x < n and 0 <= y < m and grid[x][y] == "1"
 
 
 """
