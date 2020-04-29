48c74ab0c9bd865441d5092ebd9e8dbb8977e940
depechecode <scunchen@umich.edu>
2020-04-29 18:35:14 -0400

    add triangle-cut

---
+++ two-pointers/611/611.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/valid-triangle-number/
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(len(nums)):
+            left, right = 0, i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    ans += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return ans
+
